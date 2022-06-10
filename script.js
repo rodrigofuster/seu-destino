b207ca9b87acf29ebcf3458fd461572b6eafa0a6
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-06-10 10:18:21 -0300

    add.opcoes

---
+++ script.js
@@ -0,0 +1,9 @@
+const respostas = [
+  "Certeza!",
+  "Não tenho tanta certeza.",
+  "É decididamente assim.", 
+  "Não conte com isso.",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.", 
+  "Sim, definitivamente"
+]

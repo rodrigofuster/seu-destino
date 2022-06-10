5ad055b3ab5992cee52bd475e2122f0623b9ab0b
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-06-10 18:56:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
 // criando as respostas.
+const elementoResposta = document.querySelector("#resposta");
 const respostas = [
   "Certeza!",
   "Não tenho tanta certeza.",
-  "É decididamente assim.", 
+  "É decididamente assim.",
   "Não conte com isso.",
   "Sem dúvidas!",
-  "Pergunte novamente mais tarde.", 
+  "Pergunte novamente mais tarde.",
   "Sim, definitivamente",
-  "Minha resposta é não.", 
-  "Você pode contar com isso.", 
-  "Melhor não te dizer agora.", 
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
   "A meu ver, sim.",
   "Minhas fontes dizem que não.",
   "Provavelmente.",
-  "Não é possível prever agora.", 
-  "Perspectiva boa.", 
-  "As perspectivas não são tão boas.", 
+  "Não é possível prever agora.",
+  "Perspectiva boa.",
+  "As perspectivas não são tão boas.",
   "Concentre-se e pergunte novamente.",
-  "Sim.", 
-  "Claro que não.", 
-  "Claro que sim.", 
-  "Os sinais apontam que sim.", 
-  "Os sinais apontam que não.", 
-  "Óbvio que sim.", 
-  "Óbvio que não.", 
-  "Os astros não indicam isso!", 
+  "Sim.",
+  "Claro que não.",
+  "Claro que sim.",
+  "Os sinais apontam que sim.",
+  "Os sinais apontam que não.",
+  "Óbvio que sim.",
+  "Óbvio que não.",
+  "Os astros não indicam isso!",
   "Os astros estão ao seu favor!",
-]
-// gerar numero de resposta aleátorio. 
-const totalRespostas = respostas.length; 
-const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
-
-//console.log(numeroAleatorio)
-
+];
+// gerar numero de resposta aleátorio.
 function fazerPergunta() {
-  
-
+  const totalRespostas = respostas.length;
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas);
+  elementoResposta.innerHTML = "Mostrar";
 }
